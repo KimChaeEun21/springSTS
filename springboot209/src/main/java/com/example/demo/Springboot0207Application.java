8a437b186405cd85062e092db909136c08dfdb5b
Canon <Canon@DESKTOP-PL2F7PK>
2020-02-10 15:32:45 +0900

    1

---
+++ springboot209/src/main/java/com/example/demo/Springboot0207Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0207Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0207Application.class, args);
+		
+	}
+
+}
